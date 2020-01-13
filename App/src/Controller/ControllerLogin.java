2e948f1f62f2461a051da0b24604d92a05ac464a
Tirex <ilhamsdr@gmail.com>
2020-01-13 19:35:15 +0700

    casd

--- App/src/Controller/ControllerLogin.java
+++ App/src/Controller/ControllerLogin.java
@@ -4,26 +4,35 @@
  * and open the template in the editor.
  */
 package Controller;
-import frame.Home;
+import model.User;
+import java.util.*;
 /**
  *
  * @author ilham-07010
  */
 public class ControllerLogin {
+
+   
+    public static ArrayList<User> user = new ArrayList<User>();
+    public String nama;
     
-    public static void login(String username, String pass)
+    
+//    public static void guruInsert(){
+//        user.add(new User("guru", "guru", "yanto","guru",null,null,null));
+//    }
+ 
+    
+    public static String login(String username, String pass)
     {
-        String username1 = "admin";
-        String password1 = "admin";
         
         String hasil = "gagal";
-         if(username1 == username && password1 == pass){
-         hasil = "admin";
-         Home d = new Home();
-         d.setVisible(true);
-         }
-        
-//        return hasil;
-                 
+        for (int i = 0; i<user.size(); i++)
+        {
+            if ((username == null ? user.get(i).getUsername() == null : username.equals(user.get(i).getUsername()))&& (pass == null ? user.get(i).getPassword() == null : pass.equals(user.get(i).getPassword())))
+            {   
+                if(username.equals("admin")&&pass.equals("admin")){
+                    hasil = "admin";
+          }
+        }
+        return hasil;
     }
-}
--- App/src/model/User.java
+++ App/src/model/User.java
@@ -10,5 +10,20 @@ package model;
  * @author ilham-07010
  */
 public class User {
-    
+
+    String username, password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
 }
